dc4b144af22ae0b45f351f08db3d73e6d813f991
Lionad <tangnad@qq.com>
2020-06-02 09:38:29 +0800

    utils: washObject  递归删除 Object 中 值为 Null 的键

--- utils/index.js
+++ utils/index.js
@@ -4,7 +4,24 @@ function getQuery(name, url = window.location.href) {
   return match ? match[1] : null
 }
 
+/**
+ * 递归删除 Object 中 值为 Null 的键（和 Null 数组元素）
+ * @param {Object} obj 
+ */
+function washObj(obj) {
+  const isObj = x => x instanceof Object && x !== null
+  const washKey = x => isObj(x) ? washObj(x) : x
+  return obj instanceof Array
+    ? obj.filter(x => x).map(x => washKey(x))
+    : (Object.keys(obj).map(k => {
+        obj[k] === null
+          ? delete obj[k]
+          : (obj[k] = washKey(obj[k]))
+    }), obj)
+}
+
 module.exports = {
+    washObj,
     judgeURLType (url) {
         const typeReg = {
             song: url => url.indexOf('https://music.163.com/#/song') !== -1,
