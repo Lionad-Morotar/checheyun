cec13756726c8daced656b89001f4200fd4d50ec
Lionad <tangnad@qq.com>
2020-07-22 15:08:13 +0800

    feat: query block 参数，用来获取源码对应部分内容

--- projects/main/router/wiki.js
+++ projects/main/router/wiki.js
@@ -13,10 +13,9 @@ const router = express.Router()
 // 获取 WIKI 页面实体信息的源码
 router.get('/wiki/source', (req, res) => {
     console.log('[GET-WIKI-SOURCE]')
-    const { title } = req.query
+    const { title, block } = req.query
     // TODO make createURL utils
     const uri = `https://thwiki.cc/index.php?title=${encodeURIComponent(title)}&action=edit&viewsource=1`
-
     crawler.direct({
         uri: uri,
         skipEventRequest: false,
@@ -26,7 +25,13 @@ router.get('/wiki/source', (req, res) => {
                 res.send(500).end()
             } else {
                 const $ = response.$
-                const message = $('textarea').text()
+                const content = $('textarea').text()
+                let message = content
+                if (block) {
+                    const regex = new RegExp(`[\\w\\W]*== ${block} ==([\\w\\W]+?)==`)
+                    const match = message.match(regex)
+                    message = match && match[1] || message
+                }
                 res.json({ status: 200, message })
             }
         }
